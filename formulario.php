180836e3ba382a133b97242ed4284694bac05815
Edward Orvey Ovalle Arias <42394605+eovallea3786@users.noreply.github.com>
2019-10-22 22:33:56 -0500

    php calenmd
    php calenmd

--- formulario.php
+++ formulario.php
@@ -346,7 +346,29 @@
 							<p class="textoNormal1">Fecha de ingreso a la empresa:</p>
 						</td>
 						<td width="70%">
-							meter aqui
+						<?php
+								require $KoolControlsFolder."/KoolCalendar/koolcalendar.php";
+								require $KoolControlsFolder."/KoolAjax/koolajax.php";
+								$koolajax->scriptFolder = $KoolControlsFolder."/KoolAjax";
+							 
+							 
+								$cal = new KoolCalendar("cal"); //Create calendar object
+								$cal->scriptFolder = $KoolControlsFolder."/KoolCalendar";//Set scriptFolder
+								$cal->styleFolder="default";
+							 
+								//Enable Ajax
+								$cal->AjaxEnabled = true;
+								$cal->AjaxLoadingImage = $KoolControlsFolder."/KoolAjax/loading/2.gif";
+							 
+								//Init calendar before render
+								$cal->Init();
+							?>
+ 
+
+							<div style="padding-top:20px;padding-bottom:40px;width:650px;<?php if ($style_select=="black") echo "background:#333"; ?>">
+								<?php echo $koolajax->Render();?>
+								<?php echo $cal->Render();?>
+							</div>
 						</td>
 					</tr>
 
